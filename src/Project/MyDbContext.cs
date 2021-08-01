88050e2fd9d5f929af5cd2906c3834012afa3a6b
JakeYal <30874283+JakeYal@users.noreply.github.com>
2021-08-01 19:12:50 +0100

    Initial commit

---
+++ src/Project/MyDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Project
+{
+    public class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Role>()
+                .HasMany(x => x.Permissions)
+                //We don't specify a navigation property here - this works fine in EF Core 5.0.8
+                .WithMany("Roles")
+                .UsingEntity<RolePermission>(
+                    x => x.HasOne(x => x.Permission).WithMany(),
+                    x => x.HasOne(x => x.Role).WithMany());
+        }
+    }
+}
---
+++ src/Project/RolePermissions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class RolePermission
+    {
+        public Guid PermissionId { get; set; }
+        public Guid RoleId { get; set; }
+        public Role Role { get; set; } = null!;
+        public Permission Permission { get; set; } = null!;
+    }
+}
